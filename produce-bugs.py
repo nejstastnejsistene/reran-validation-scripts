753f1613313595c5ffe0029bb026baf2c7b953ac
Peter Johnson <peter@peterjohnson.io>
2014-05-07 15:14:19 -0400

    Successful kill, start, and then scan an app for crashes.

--- produce-bugs.py
+++ produce-bugs.py
@@ -1,13 +1,56 @@
+import re
+import subprocess
 from commands import getoutput
 
-def get_pid(package):
-    lines = getoutput('adb shell ps | grep {}'.format(package)).split('\n')
-    if len(lines) > 1:
-        raise Exception, 'More than one process for {}'.format(package)
-    try:
-        return int(lines[0].split()[1])
-    except:
-        raise Exception, 'Unable to find pid for {}'.format(package)
+
+class LogcatScanner(object):
+
+    command = 'adb logcat'.split()
+    pattern = re.compile(r'^[EF]/(\w+)\(\s*(\d+)\)')
+
+    def __init__(self, pid):
+        self.scanning = False
+        self.pid = pid
+
+    def start(self):
+        assert not self.scanning, 'Already scanning!'
+        self.scanning = True
+        getoutput('adb shell logcat -c') # Clear the logcat.
+        p = subprocess.Popen(self.command, stdout=subprocess.PIPE)
+        while self.scanning:
+            line = p.stdout.readline()
+            m = self.pattern.match(line)
+            if m is None:
+                continue
+            tag, pid = m.groups()
+            if tag == 'AndroidRuntime' and int(pid) == self.pid:
+                break
+
+    def stop(self):
+        self.scanning = False
+
+
+def get_pids(package):
+    lines = getoutput("adb shell ps | grep {} | awk '{{print $2}}'".format(package))
+    for pid in lines.split():
+        yield int(pid)
 
 if __name__ == '__main__':
-    print `get_pid('com.nerdyoctopus.gamedots')`
+    import sys, time
+    package = sys.argv[1]
+
+    # Kill the package if it's running, and the restart it.
+    for pid in get_pids(package):
+        print 'Killing PID', pid
+        getoutput('adb shell su -c kill -9 {}'.format(pid))
+
+    # Start the app.
+    print getoutput('adb shell am start {}'.format(package))
+    time.sleep(1)
+
+    # Get the pid for the newly started app.
+    pid = next(get_pids(package))
+
+    # Scan for errors.
+    print 'Scanning for PID {}'.format(pid)
+    LogcatScanner(pid).start()
